9ab3cfaa6bf8cb90d447c1139c1a7cade7955a80
Vistril <38549907+Vistril@users.noreply.github.com>
2019-10-30 18:22:10 -0600

    Add files via upload

---
+++ songfixer.js
@@ -0,0 +1,10 @@
+const fs = require('fs');
+const writeTo = './formattedsong.txt';
+
+fs.watch('./', function (event, filename) {
+    if (filename != 'currentsong.txt') return;
+    let regex = /(<([^>]+)>)/ig
+    let result = fs.readFileSync('./currentsong.txt', 'utf8').replace(regex, ''); 
+    fs.writeFileSync(writeTo, result);
+});
+
